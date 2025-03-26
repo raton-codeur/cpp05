2f19f70509953e21a439d0ad73a1c3286c9c080e
qhauuy <qhauuy@student.42mulhouse.fr>
2025-03-26 09:11:28 +0100

    blabla

--- repo/ex00/inc/Bureaucrat.hpp
+++ repo/ex00/inc/Bureaucrat.hpp
@@ -8,16 +8,19 @@ class Bureaucrat
 	private :
 		const std::string	_name;
 		int					_grade;
+
 	public :
-							Bureaucrat();
-							Bureaucrat(const std::string& name, int grade);
-							Bureaucrat(const Bureaucrat& other);
-		Bureaucrat&			operator=(const Bureaucrat& other);
-							~Bureaucrat();
+					Bureaucrat();
+					Bureaucrat(const std::string& name, int grade);
+					Bureaucrat(const Bureaucrat& other);
+		Bureaucrat&	operator=(const Bureaucrat& other);
+					~Bureaucrat();
+
 		const std::string&	getName() const;
 		int					getGrade() const;
-		void				incrementGrade();
-		void				decrementGrade();
+
+		void	incrementGrade();
+		void	decrementGrade();
 
 		class GradeTooHighException : public std::exception
 		{
