46d20024b7a621f36449a47c70483c935b1f3c4e
Quentin Hauuy <quentinhauuy@hotmail.com>
2025-03-26 11:16:37 +0100

    blabla

--- repo/ex02/src/PresidentialPardonForm.cpp
+++ repo/ex02/src/PresidentialPardonForm.cpp
@@ -1,2 +1,31 @@
 #include "PresidentialPardonForm.hpp"
 
+PresidentialPardonForm::PresidentialPardonForm() :
+	AForm("PresidentialPardonForm", 25, 5),
+	_target("default_target")
+{}
+
+PresidentialPardonForm::PresidentialPardonForm(const std::string& target) :
+	AForm("PresidentialPardonForm", 25, 5),
+	_target(target)
+{}
+
+PresidentialPardonForm::PresidentialPardonForm(const PresidentialPardonForm& other) :
+	AForm(other),
+	_target(other._target)
+{}
+
+PresidentialPardonForm& PresidentialPardonForm::operator=(const PresidentialPardonForm& other)
+{
+	AForm::operator=(other);
+	return *this;
+}
+
+PresidentialPardonForm::~PresidentialPardonForm()
+{}
+
+void PresidentialPardonForm::execute(const Bureaucrat& b) const
+{
+	checkExecution(b);
+	std::cout << _target << " has been pardoned by Zaphod Beeblebrox" << std::endl;
+}
--- repo/ex02/src/main.cpp
+++ repo/ex02/src/main.cpp
@@ -1,11 +1,22 @@
 #include "Bureaucrat.hpp"
 #include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
 
 int main()
 {
-	ShrubberyCreationForm f;
+	srand(time(NULL));
+
+	// RobotomyRequestForm f("john");
+	// std::cout << f << std::endl;
+	// Bureaucrat b("bob", 20);
+	// b.signForm(f);
+	// for (int i = 0; i < 10; i++)
+	// 	b.executeForm(f);
+
+	PresidentialPardonForm f("john");
 	std::cout << f << std::endl;
-	Bureaucrat b("bob", 20);
+	Bureaucrat b("bob", 5);
 	b.signForm(f);
 	b.executeForm(f);
 }
