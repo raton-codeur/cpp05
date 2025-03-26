81123f3d1c1ef07032f320a8653132fdfa7bd3f7
Quentin Hauuy <quentinhauuy@hotmail.com>
2025-03-26 09:21:40 +0100

    blabla

--- repo/ex01/inc/Form.hpp
+++ repo/ex01/inc/Form.hpp
@@ -10,16 +10,18 @@ class Form
 		const int			_requiredGradeToSign;
 		const int			_requiredGradeToExecute;
 	public :
-							Form();
-							Form(const std::string& name, int requiredGradeToSign, int requiredGradeToExecute);
-							Form(const Form& other);
-		Form&				operator=(const Form& other);
-							~Form();
+				Form();
+				Form(const std::string& name, int requiredGradeToSign, int requiredGradeToExecute);
+				Form(const Form& other);
+		Form&	operator=(const Form& other);
+				~Form();
+
 		const std::string&	getName() const;
 		bool				isSigned() const;
 		int					getRequiredGradeToSign() const;
 		int					getRequiredGradeToExecute() const;
-		void				beSigned(const Bureaucrat& b);
+
+		void beSigned(const Bureaucrat& b);
 
 		class GradeTooHighException : public std::exception
 		{
