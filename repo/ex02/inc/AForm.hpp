6e4782cea6984e005826ca92db791d6c2458b501
Quentin Hauuy <quentinhauuy@hotmail.com>
2025-03-26 09:47:36 +0100

    blabla

--- repo/ex02/inc/AForm.hpp
+++ repo/ex02/inc/AForm.hpp
@@ -9,18 +9,22 @@ class AForm
 		bool				_signed;
 		const int			_requiredGradeToSign;
 		const int			_requiredGradeToExecute;
+
 	public :
-							AForm();
-							AForm(const std::string& name, int requiredGradeToSign, int requiredGradeToExecute);
-							AForm(const AForm& other);
-		AForm&				operator=(const AForm& other);
-		virtual				~AForm() = 0;
+				AForm();
+				AForm(const std::string& name, int requiredGradeToSign, int requiredGradeToExecute);
+				AForm(const AForm& other);
+		AForm&	operator=(const AForm& other);
+		virtual	~AForm() = 0;
+
 		const std::string&	getName() const;
 		bool				isSigned() const;
 		int					getRequiredGradeToSign() const;
 		int					getRequiredGradeToExecute() const;
-		void				beSigned(const Bureaucrat& b);
-		virtual void		execute(const Bureaucrat& b) const;
+
+		void beSigned(const Bureaucrat& b);
+
+		virtual void execute(const Bureaucrat& b) const;
 
 		class GradeTooHighException : public std::exception
 		{
--- repo/ex02/inc/ShrubberyCreationForm.hpp
+++ repo/ex02/inc/ShrubberyCreationForm.hpp
@@ -6,13 +6,15 @@ class ShrubberyCreationForm : public AForm
 {
 	private :
 		const std::string _target;
+
 	public :
 								ShrubberyCreationForm();
 								ShrubberyCreationForm(const std::string& target);
 								ShrubberyCreationForm(const ShrubberyCreationForm& other);
 		ShrubberyCreationForm&	operator=(const ShrubberyCreationForm& other);
 								~ShrubberyCreationForm();
-		void					execute(const Bureaucrat& b) const;
+
+		void execute(const Bureaucrat& b) const;
 };
 
 #endif
